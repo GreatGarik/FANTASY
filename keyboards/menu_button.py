7223e806adad983a97eb78531ce784a499806451
Igor Perovsky <greatgarik@mail.ru>
2024-11-22 12:15:14 +0300

    Modified registration form and implemented new validation features

--- keyboards/menu_button.py
+++ keyboards/menu_button.py
@@ -5,9 +5,6 @@ from aiogram.types import BotCommand
 # Функция для настройки кнопки Menu бота
 async def set_main_menu(bot: Bot):
     main_menu_commands = [BotCommand(
-        command='/start',
-        description='На старт!'),
-        BotCommand(
             command='/predict',
             description='Сделать прогноз на ближайший GP'),
         BotCommand(
