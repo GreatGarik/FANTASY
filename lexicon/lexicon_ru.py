153b061c37a06c25f3cbff5c3aaf1adf6fc3af5f
Igor Perovsky <greatgarik@mail.ru>
2024-11-22 12:29:13 +0300

    Modified registration form and implemented new validation features

--- lexicon/lexicon_ru.py
+++ lexicon/lexicon_ru.py
@@ -1,5 +1,5 @@
 LEXICON_RU = {'yes_button': ' Мой ответ да',
               'start_answer': 'Добро пожаловать в бот Fantasy F1, для регистрации отправьте команду /registration',
               'help_answer': 'Я принимаю прогнозы на Фэнтези',
-              'presict_answer': 'Прогнозик делаем',
+              'predict_answer': 'Прогнозик делаем',
               'unknown_command': 'Используйте кнопки меню (Эта кнопка еще не работает)'}
