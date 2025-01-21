2754353262d7a426f3cb91e17843ff3cff731557
Igor Perovsky <greatgarik@mail.ru>
2025-01-21 17:13:02 +0300

    Fix: max laps == 19

--- dialogs/dop_functions.py
+++ dialogs/dop_functions.py
@@ -1,8 +1,17 @@
 from aiogram import Bot
+import logging
+
+# Инициализируем логгер
+logger = logging.getLogger(__name__)
 
 async def send_message(user_id: int, text: str, bot: Bot):
+    logging.basicConfig(
+        level=logging.INFO,
+        filename="py_log.log", filemode="a",
+        format=u'%(filename)s:%(lineno)d #%(levelname)-8s '
+               u'[%(asctime)s] - %(name)s - %(message)s')
     try:
         await bot.send_message(user_id, text)
     except Exception as e:
-        print(e)
+        logger.warning(f'{e}, {user_id}')
 
