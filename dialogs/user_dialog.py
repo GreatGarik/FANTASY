51f3d148990b899d6464ecb1ab290436b86fad6c
GreatGarik <greatgarik@mail.ru>
2025-02-19 09:48:33 +0300

    add req

--- dialogs/user_dialog.py
+++ dialogs/user_dialog.py
@@ -37,7 +37,7 @@ user_dialog = Dialog(
                 id='button_about',
                 on_click=button_about
             ),
-            Url(text=Const('Чат F1 Fantasy by Silly Formula'),
+            Url(text=Const('💬 Чат F1 Fantasy by Silly Formula'),
                 url=Const('https://t.me/+fa8OXqqblLxhODQy'),
                 id='chat_tg_group'),
             Button(
