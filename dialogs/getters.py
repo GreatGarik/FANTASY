eb4d06423e2498dac1ed1ea6e4840d9f074502fc
GreatGarik <greatgarik@mail.ru>
2025-02-26 16:48:54 +0300

    timing

--- dialogs/getters.py
+++ dialogs/getters.py
@@ -680,6 +680,7 @@ async def button_confirm_predict(callback: CallbackQuery, button: Button, dialog
     await add_scheduled_message(0, text, datetime.strptime(time_penalty, "%Y-%m-%d %H:%M") - timedelta(hours=24))
     scheduler.add_job(schedule_message, 'date', run_date=(datetime.strptime(time_penalty, "%Y-%m-%d %H:%M") - timedelta(hours=12)).strftime("%Y-%m-%d %H:%M:%S"), args=[0, text, bot])
 
+
     '''
     # Уведомление, что осталось 4 часа до штрафа
     text = f'⏱️ Осталось 4 часа, чтобы подать прогноз на <b> {await get_name_gp(gp_id)} GP</b>\nбез штрафа до <b>{get_day_of_week(time_penalty_d.weekday(), "родительный")} {time_penalty} МСК</b>\nОкончание приёма прогнозов в <b>{get_day_of_week(time_end_d.weekday(), "винительный")} {time_end} МСК</b>'
