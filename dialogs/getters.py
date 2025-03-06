64dd09d124a3e01a05cd2af9f30db5bd8647864f
GreatGarik <greatgarik@mail.ru>
2025-03-06 12:37:41 +0300

    Added info about racers by race, quali, etc

--- dialogs/getters.py
+++ dialogs/getters.py
@@ -188,8 +188,9 @@ async def button_unban_user(callback: CallbackQuery, button: Button,
 
 
 async def new_name_user(message: Message, widget: ManagedTextInput, dialog_manager: DialogManager, text: str) -> None:
-    await change_user_name_async(int(dialog_manager.dialog_data['user_tg_id']), text)
-    await message.answer(f'Вы изменили имя на {text}')
+    name, lastname = text.split()
+    await change_user_name_async(int(dialog_manager.dialog_data['user_tg_id']), name.capitalize() + ' ' + lastname.upper())
+    await message.answer(f'Вы изменили имя на {name.capitalize() + ' ' + lastname.upper()}')
     dialog_manager.dialog_data.clear()
     await dialog_manager.switch_to(AdminSG.users_menu)
 
